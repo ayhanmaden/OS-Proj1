af6a7f359771735e956a7158ff6ed1ae1898ccaa
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-11-21 17:52:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,50 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LIMIT 100
-int main()
-{
-	// insert , calculate , readall , clear , exit 
-	char str[MAX_LIMIT];
-	fgets(str, MAX_LIMIT, stdin);
-	printf("%s", str);
-	char* str2 = (char*)malloc(sizeof(char) * MAX_LIMIT);
-	strcpy(str2, str);
-	char delim[] = " ";
-	int  number[10], i = 0, size = 0;
-	char* ptr = strtok(str2, delim);
-
-	while (ptr != NULL)
+#define  MAX_LIMIT 100
+char* str3[MAX_LIMIT];
+void inputword(char* input);// get word from user input;;
+void main() {
+	// insert , calculate , readall , clear , exit
+	char str[MAX_LIMIT]; // limit input 
+	while (1)
 	{
-		printf("%s\n", ptr);
-		if (size >= 2) {
-			number[i] = atoi(ptr);
-			i++;
+		printf("\naccountant>>");
+		fgets(str, MAX_LIMIT, stdin); // input from user 
+		inputword(str);// full array 
+		if ((strcmp(str3[0], "insert")) == 0) {
+			printf("insert cmd ");
 		}
-		size++;
+		else if ((strcmp(str3[0], "calculate")) == 0) {
+			printf("calculate cmd ");
+		}
+		else if ((strcmp(str3[0], "readall\n")) == 0) {
+			printf("readall cmd ");
+		}
+		else if ((strcmp(str3[0], "clear\n")) == 0) {
+			system("cls");
+		}
+		else if ((strcmp(str3[0], "exit\n")) == 0) {
+			exit(0);
+		}
+		else { printf("%s : command not found ",str); }
+		
+	}
+}
+
+void inputword(char* input)
+{
+	char* str2 = (char*)malloc(sizeof(char) * MAX_LIMIT); // char str2[MAX_LIMIT]
+	strcpy(str2, input); // string make cp
+	char delim[] = " ";// splite 
+	char* ptr = strtok(str2, delim);// string ptr[]=split(" "); 
+	for (int i = 0;1; i++)
+	{
+		if (ptr==NULL) {
+			break;
+		}
+		str3[i] = ptr;
 		ptr = strtok(NULL, delim);
 	}
-
-	for (int k = 0; k < i; k++) {
-		printf(" ===> %d \n ", number[k]);
-	}
-
-	return 0;
+	
 }
