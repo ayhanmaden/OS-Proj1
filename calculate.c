29576aecbb635f4ce417fbc8518f6a75be957400
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-11-26 21:53:35 +0300

    Update calculate.c

--- calculate.c
+++ calculate.c
@@ -6,28 +6,33 @@
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {
-	for (int i=0; i<3; i++) 
-    { 
-        printf("[%s\n]",argv[i]); 
-    } 
-    FILE* ptr = fopen("database","r"); 
-    /* Assuming that abc.txt has content in below 
-       format 
-       1
-       2
-       3
-       4
-       5
-        */
-    char* buf[100];
-    int cut=0,sum=0; 
-    while (fscanf(ptr,"%d",buf)==1){
-        if(cut==atoi(argv[1]) || cut==atoi(argv[2]) ){
-          sum=sum+atoi(buf);
-        }
-        cut++;
-        printf("amk => %d \n", cut); 
+    FILE *myFile;
+    myFile = fopen("database", "r");
+    int numberArray[atoi(argv[3]) - 1];
+    int i;
+    for (i = 0; i < atoi(argv[3]) - 1; i++)
+    {
+        fscanf(myFile, "%d", &numberArray[i]);
+    }
 
-    } 
-	return 0;
+    if (strcmp(argv[0], "-s") == 0)
+    {  
+        int sum=(numberArray[atoi(argv[1])]+numberArray[atoi(argv[2])]);
+        puts(" ");
+        printf("sum ==> %d + %d = %d",
+        numberArray[atoi(argv[1])-1],
+        numberArray[atoi(argv[2])-1],
+        sum);
+    }
+    else if (strcmp(argv[0], "-r") == 0)
+    {
+        int sum=0;
+        for (i = atoi(argv[1]); i <atoi(argv[2]); i++)
+        {
+            printf("Number is: %d\n\n", numberArray[i]);
+            sum = sum+ numberArray[i];
+        }
+        puts(" ");
+        printf("sum %d ",sum);
+    }
 }
