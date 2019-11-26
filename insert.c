e9e4c2c1867f003cb461fc8f384446bc6a85fd5e
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-11-26 18:10:53 +0300

    Update insert.c

--- insert.c
+++ insert.c
@@ -1,12 +1,18 @@
-
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
-int main(int argc, char *argv[]){
-              for (int i = 0; i <10; i++)
-		{
-			printf("%s -->  ",argv[i]);
-
-		}
-    return 0;
+int main(int argc, char *argv[])
+{
+	int i; 
+    //open file sample.txt in write mode 
+    FILE *fptr = fopen("database", "w"); 
+    for (i=1; i<atoi(argv[0]); i++) 
+    { 
+        fprintf(fptr,"%d\n",atoi(argv[i])); 
+    } 
+    fclose(fptr); 
+	return 0;
 }
