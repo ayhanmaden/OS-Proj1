e47741f094f90e554cfe0a7706d77c6f1e6ce0ab
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-11-28 19:40:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -19,9 +19,9 @@ void main(int argc, char **argv, char **envp)
 		fgets(str, MAX_LIMIT, stdin); // input from user
 		inputword(str);				  // full array
 		if ((strcmp(str3[0], "insert")) == 0)
-		{	
+		{
 			int k = 1;
-            for (int i = 1; i < 100; i++) // negative numbers contorl
+			for (int i = 1; i < 100; i++) // negative numbers contorl
 			{
 				if (str3[i] != NULL)
 				{
@@ -29,14 +29,14 @@ void main(int argc, char **argv, char **envp)
 					k++;
 				}
 			}
-			for(int d =0;d<100;d++ ){
-               str3[d]=NULL;
+			for (int d = 0; d < 100; d++)
+			{
+				str3[d] = NULL;
 			}
 			char snum[2];
 			sprintf(snum, "%d", k);
 			pozitif[0] = snum;
 			mathods(1);
-			
 		}
 		else if ((strcmp(str3[0], "calculate")) == 0)
 		{
@@ -46,13 +46,12 @@ void main(int argc, char **argv, char **envp)
 				calc[0] = str3[1];
 				calc[1] = str3[2];
 				calc[2] = str3[3];
-			
 			}
 			else if ((strcmp(str3[1], "-s") == 0))
 			{
-				calc[0] = str3[1];	// 	-s
-				calc[1] = str3[2];	// ilk sayi
-				calc[2] = str3[3];	// ikinci sayi
+				calc[0] = str3[1]; // 	-s
+				calc[1] = str3[2]; // ilk sayi
+				calc[2] = str3[3]; // ikinci sayi
 			}
 			else
 			{
@@ -62,7 +61,22 @@ void main(int argc, char **argv, char **envp)
 		}
 		else if ((strcmp(str3[0], "readall\n")) == 0)
 		{
-			printf("readall cmd ");
+
+			static const char filename[] = "database";
+			FILE *file = fopen(filename, "r");
+			if (file != NULL)
+			{
+				char line[128];								   /* or other suitable maximum line size */
+				while (fgets(line, sizeof line, file) != NULL) /* read a line */
+				{
+					fputs(line, stdout); /* write the line */
+				}
+				fclose(file);
+			}
+			else
+			{
+				perror(filename); /* why didn't the file open? */
+			}
 		}
 		else if ((strcmp(str3[0], "clear\n")) == 0)
 		{
