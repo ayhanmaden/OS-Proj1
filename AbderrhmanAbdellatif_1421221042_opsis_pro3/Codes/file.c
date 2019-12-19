e0757ba057ccc8fa13c0dfd1db5d02f2879deb2b
taha <dexter196tr@gmail.com>
2019-12-20 02:37:00 +0300

    pipe

--- AbderrhmanAbdellatif_1421221042_opsis_pro3/Codes/file.c
+++ AbderrhmanAbdellatif_1421221042_opsis_pro3/Codes/file.c
@@ -10,43 +10,49 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 #define MAX 256
-char *convert(int number)
+int strtoint(char *charnums)
 {
-    int count = 0;
-    int numbertmp = number;
-    while (numbertmp > 0)
+    int number = 0;
+    int index = 0;
+    while (charnums[index])
     {
-        int digit = numbertmp % 10;
-        numbertmp /= 10;
-        count++;
-    }
-    char *value = (char *)calloc(count, sizeof(unsigned char));
-    numbertmp = number;
-    count--;
-    while (numbertmp > 0)
-    {
-        int digit = numbertmp % 10;
-        numbertmp /= 10;
-        char x = (char)48 + digit;
-        *(value + count) = x;
-        count--;
-    }
+        if (('0' <= charnums[index]) && (charnums[index] <= '9'))
+        {
 
-    return value;
+            if (!number)
+                number = ((int)charnums[index]) - 48;
+            else
+            {
+                number = (number *= 10) + ((int)charnums[index] - 48);
+            }
+            index++;
+        }
+        else
+        {
+            number = -1;
+            printf("\nGecersiz islem");
+            break;
+        }
+    }
+    return number;
 }
-int main()
+int main(int argc, char *argv[])
 {
     FILE *fptr1, *fptr2;
     int size = 0, c = 0, i = 0;
     char str[MAX];
     char newln[MAX], temp[] = "temp.txt";
     static const char fname[] = "test.txt";
-    char *newargv[3];
     int forkdurum = 0;
     int pipe_p[2];
     if (pipe(pipe_p) < 0)
+    {
         printf("can not create  a pipe");
+    }
+    pipe_p[0] = strtoint(argv[0]);
+    pipe_p[1] = strtoint(argv[1]);
 
     while (1)
     {
@@ -57,21 +63,15 @@ int main()
         {
             printf("file is empty\n");
             printf("lines file is : 0. \n");
-            sleep(10);
+            sleep(5);
         }
         else
         {
-            if (!fptr1)
-            {
-                printf("Unable to open the input file!!\n");
-                return 0;
-            }
             // size of line
             FILE *myFile;
             myFile = fopen("test.txt", "r");
             int lines = 0;
             char ch = 0;
-
             while (!feof(myFile))
             {
                 ch = fgetc(myFile);
@@ -81,9 +81,8 @@ int main()
                 }
             }
             printf("lines file %d\n", lines);
-            int numberArray[lines][4]; // array
+            int numberArray[lines]; // array
             int i = 0;
-            char *numbers;
             static const char filename[] = "test.txt";
             FILE *file = fopen(filename, "r");
             if (file != NULL)
@@ -92,63 +91,38 @@ int main()
                 while (fgets(line, sizeof(line), file) != NULL) /* read a line */
                 {
                     fputs(line, file);
-                    numberArray[i][4] = atoi(line); /* write to array  */
+                    numberArray[i] = atoi(line); /* write to array  */
                     i++;
                 }
                 fclose(file);
             }
-
             fptr2 = fopen(temp, "w");
-            if (!fptr2)
-            {
-                printf("Unable to open a temporary file to write!!\n");
-                fclose(fptr1);
-                return 0;
-            }
-            /* get the new line from the user */
             strcpy(newln, "");
-            /* get the line number to delete the specific line */
-            // copy all contents to the temporary file other except specific line
             while (!feof(fptr1))
             {
                 strcpy(str, "\0");
                 fgets(str, MAX, fptr1);
             }
+
             fclose(fptr1);
             fclose(fptr2);
             remove(fname);
             rename(temp, fname);
             printf("successfully..!! \n");
-            char str[15];
-            sprintf(str, "%d", lines);
-            newargv[0] = "convert(pipe_p[0])";
-            newargv[1] = "convert(pipe_p[1])";
-            newargv[2] = str;
-            newargv[3] = NULL;
-            if ((forkdurum = fork()) < 0)
+            close(pipe_p[0]);
+
+            for (int i = 0; i < lines; i++)
             {
-                printf("can't create  a fork ");
-            }
-            else if ((forkdurum = fork()) == 0)
-            {
-                c = execv("main", newargv);
-            }
-            else if (forkdurum > 0)
-            {
-                 close(pipe_p[0]);
-                for (int i = 0; i < lines; i++)
+                if (write(pipe_p[1], numberArray, sizeof(numberArray)) < 0) // write in pipe
                 {
-                    if (write(pipe_p[1], numberArray, sizeof(numberArray)) < 0) // write in pipe
-                    {
-                        printf("cant write in to pipe  ");
-                    }
-                    printf("write %d\n", numberArray[i][0]);
+                    printf("cant write in to pipe  ");
                 }
-                
-                wait(&c);
+                printf("write %d\n", numberArray[i]);
+                sleep(2);
             }
+            exit(0);
+        sleep(5);
         }
-        sleep(10);
     }
     return 0;
 }
--- AbderrhmanAbdellatif_1421221042_opsis_pro3/Codes/main.c
+++ AbderrhmanAbdellatif_1421221042_opsis_pro3/Codes/main.c
@@ -10,28 +10,70 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-int main(int argc, char *argv[])
+#include <string.h>
+char *convert(int number)
 {
+    int count = 0;
+    int numbertmp = number;
+    while (numbertmp > 0)
+    {
+        int digit = numbertmp % 10;
+        numbertmp /= 10;
+        count++;
+    }
+    char *value = (char *)calloc(count, sizeof(unsigned char));
+    numbertmp = number;
+    count--;
+    while (numbertmp > 0)
+    {
+        int digit = numbertmp % 10;
+        numbertmp /= 10;
+        char x = (char)48 + digit;
+        *(value + count) = x;
+        count--;
+    }
 
-    int pipe_p[2];
-    int i;
-    int lines = atoi(argv[2]);
-    int numberArray[lines][4];
-    printf("lines : %d \n",lines);
-    if (pipe(pipe_p) < 0)
+    return value;
+}
+int main()
+{
+    int pipefd[2];
+    int pid;
+    int i, line;
+    char s[1000];
+    int c;
+    int numberArray[10];
+
+    if (pipe(pipefd) < 0)
     {
         perror("pipe");
         exit(1);
     }
-    if (read(pipe_p[0],numberArray, sizeof(numberArray)) < 0)
-    {
-        printf("C: Can't read ");
-    }
-    
-    for (int j = 0; j < lines; j++)
-            {
-                printf(" aliyor %d \n", numberArray[j][4]);
-            }
+    char *newargv[2];
+    newargv[0] = convert(pipefd[0]);
+    newargv[1] = convert(pipefd[1]);
+    newargv[2] = NULL;
+    pid = fork();
 
+    if (pid == 0)
+    {
+
+        c = execv("reader", newargv);
+        perror("");
+        close(pipefd[1]);
+    }
+    else
+    {
+
+        wait(&c);
+        printf("reading from pipe ...\n");
+        read(pipefd[0], numberArray, sizeof(numberArray));
+        for (int i = 0; i < 10; i++)
+        {
+            printf("read from pipe => %d\n", numberArray[i]);
+        }
+        
+    }
     return 0;
 }
+
